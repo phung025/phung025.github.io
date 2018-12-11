49997adae3cfc9023aaea247e555590eb20e149a
phung025 <phungle.thahnam@gmail.com>
2018-12-10 23:27:53 -0600

    update content

--- content/data/footer.rkt
+++ content/data/footer.rkt
@@ -1,9 +1,18 @@
 #lang racket
 
+(require gregor)
+
 (provide last-update
          racket-url
          page-generator-url)
 
-(define last-update "Oct 29, 2018")
+(define last-update
+  (let* [(calendar (parameterize ([current-clock (lambda () (current-posix-seconds))])
+                     (list (today/utc) (current-time/utc))))
+         (date (list-ref calendar 0))
+         (time (list-ref calendar 1))]
+    (parameterize ([current-locale "en"])
+      (string-append (~t time "hh:mm:ss a") " - " (~t date "EEEE, MMMM d, y G")))))
 (define racket-url "https://www.racket-lang.org/")
 (define page-generator-url "https://github.com/phung025/phung025.github.io")
+
