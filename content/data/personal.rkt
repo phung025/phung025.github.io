057f844adac36f031508a6796c5099c5cd2eb7bb
Nam Phung <phungle.thanhnam@gmail.com>
2024-07-27 17:48:05 -0500

    new UI

--- content/data/personal.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-;; This file contains the content for displaying
-;; the personal introduction on the page
-
-(provide personal-introduction)
-
-(define personal-introduction "When not working, I love to watch movies and read books. As a matter of fact, I'm a big fan of The Lord of The Rings (both the book and the movie). Besides that, my hobby also includes personal finance. I also like playing games (RuneScape, Lord of The Rings Online, etc) and watching soccer.")
